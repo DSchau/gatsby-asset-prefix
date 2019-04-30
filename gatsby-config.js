df6e55bc96ccee3f4a3aae2773d7a53f6d798d7a
Dustin Schau <DSchau@users.noreply.github.com>
2019-04-30 16:15:12 -0500

    chore: update to latest changes, including sitemap (#1)
    * chore: update all deps
    
    * feat: finish
    
    * chore: add an rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   assetPrefix: `https://gatsby-asset-prefix-cdn.netlify.com`,
   pathPrefix: `/blog`,
   siteMetadata: {
+    siteUrl: `https://gatsby-asset-prefix.netlify.com`,
     title: 'Gatsby Default Starter',
   },
   plugins: [
@@ -27,6 +28,45 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            query: `
+              {
+                pages: allSitePage {
+                  nodes {
+                    path
+                  }
+                }
+              }
+            `,
+            serialize({ query: { pages, site } }) {
+              return pages.nodes.map(node => {
+                return {
+                  description: `yadda yadda`,
+                  date: `10-05-2019`,
+                  url: `${site.siteMetadata.siteUrl}${node.path}`
+                }
+              })
+            },
+            output: '/rss.xml'
+          }
+        ]
+      }
+    }
     // `gatsby-plugin-offline` // bye bye for nwo
   ],
 }
